93014c73ec7a7ea544c1e15e6a4cbe9aadbd7251
Sreedhar <srajendran@yapstone.com>
2020-12-24 13:25:42 +0530

    add

--- src/com/learn/string/MaxDepthBinaryTree.java
+++ src/com/learn/string/MaxDepthBinaryTree.java
@@ -1,40 +1,44 @@
 package com.learn.string;
 
+import java.util.LinkedList;
+
 /**
-Given the root of a binary tree, return its maximum depth.
-A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-                3
-              /   \
-             9    20
-                /    \
-               15     7
-                    
-Example 1:
-Input: root = [3,9,20,null,null,15,7]
-Output: 3
-Example 2:
-Input: root = [1,null,2]
-Output: 2
-Example 3:
-Input: root = []
-Output: 0
-Example 4:
-Input: root = [0]
-Output: 1
-*/
+ * Given the root of a binary tree, return its maximum depth.
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *       3
+ *     /   \
+ *    9    20
+ *   /       \
+ *  15        7
+ *
+ *
+ * <p>
+ * Example 1:
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * Example 2:
+ * Input: root = [1,null,2]
+ * Output: 2
+ * Example 3:
+ * Input: root = []
+ * Output: 0
+ * Example 4:
+ * Input: root = [0]
+ * Output: 1
+ */
 
 class MaxDepthBinaryTree {
     public int maxDepth(TreeNode root) {
         LinkedList<TreeNode> stack = new LinkedList<>();
         LinkedList<Integer> depths = new LinkedList<>();
-        if(root == null) return 0;
+        if (root == null) return 0;
         stack.add(root);
         depths.add(1);
         int depth = 0, current_depth = 0;
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             root = stack.pollLast();
             current_depth = depths.pollLast();
-            if(root != null) {
+            if (root != null) {
                 depth = Math.max(depth, current_depth);
                 stack.add(root.left);
                 stack.add(root.right);
@@ -46,17 +50,21 @@ class MaxDepthBinaryTree {
     }
 }
 
-public class TreeNode {
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { 
-      this.val = val; 
+
+    TreeNode() {
     }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
     TreeNode(int val, TreeNode left, TreeNode right) {
-      this.val = val;
-      this.left = left;
-      this.right = right;
-   }
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
